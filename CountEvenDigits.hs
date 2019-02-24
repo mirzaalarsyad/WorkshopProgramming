0d4144f35e0a8780483ff22dade76d6ac589403b
mirzaalarsyad <mirza.fathan@gmail.com>
2019-02-24 15:06:49 +0700

    third

--- CountEvenDigits.hs
+++ CountEvenDigits.hs
@@ -7,9 +7,11 @@ module CountEvenDigits where
 -- HITUNG DIGIT GENAP                             countEvenDigits(n)
 -- DEFINISI DAN SPESIFIKASI
 countEvenDigits :: Int -> Int
+print a :: Int -> [Char]
 -- { Menerima masukan 1 nilai integer positif. Realisasi fungsi countEvenDigits(n) menghasilkan banyaknya digit bilangan tersebut yang genap (0, 2, 4, 6, atau 8) }
 -- REALISASI
 countEvenDigits n
     | div n 10 == 0 = if (mod n 2 == 0) then 1 else 0 --basis
     | otherwise =   if (mod n 2 == 0) then 1 + (countEvenDigits (div n 10)) --recc
                     else (countEvenDigits (div n 10))
+print a = "abcdef"
